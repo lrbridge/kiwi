881e7fd5f7026bd97882a474cd24d7349afc53b0
Lauren Bridge <lrbridge@umich.edu>
2015-10-28 19:25:46 -0700

    initial commit

---
+++ test/part1/Part1Test.java
@@ -0,0 +1,13 @@
+package part1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Part1Test {
+
+    @Test
+    public void testSolve() throws Exception {
+        assertEquals(new Part1().solve(), 2);
+    }
+}
